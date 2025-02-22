1b14cd6ff56aeebfd4e3b6482043942be0130157
karthikEswaran05 <karthik.eswaran@hashagile.com>
2025-02-22 20:26:46 +0530

    deploy data

--- web_app/src/App.tsx
+++ web_app/src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-// Create a query client instance
 const queryClient = new QueryClient();
 
 function App() {
--- web_app/src/components/Login.tsx
+++ web_app/src/components/Login.tsx
@@ -1,10 +1,7 @@
-import React from 'react'
 
 const Login = () => {
   return (
     <div>
-
-    
       <h1>Login age</h1>
     </div>
   )
