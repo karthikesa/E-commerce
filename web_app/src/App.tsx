e61513ac4e83fdf50d510cbf2107dc200309817b
karthikEswaran05 <karthik.eswaran@hashagile.com>
2025-02-22 20:10:14 +0530

    [install package]

--- web_app/src/App.tsx
+++ web_app/src/App.tsx
@@ -1,10 +1,20 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+// Create a query client instance
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <h1>Shifu karthik</h1>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
---
+++ web_app/src/components/Login.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Login age</h1>
+    </div>
+  )
+}
+
+export default Login
